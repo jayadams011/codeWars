890e815b08c74db2f1b86945a968c9a8233382a2
jayadams011 <j.adams011@hotmail.com>
2018-01-10 08:54:30 -0800

    finishedkata

--- My-head-is-on-the-wrong-end/readme.md
+++ My-head-is-on-the-wrong-end/readme.md
@@ -3,5 +3,7 @@ https://www.codewars.com/kata/my-head-is-at-the-wrong-end/train/javascript
 
 solution url
 
+https://www.codewars.com/kata/my-head-is-at-the-wrong-end/discuss
+
 repo url
 
--- My-head-is-on-the-wrong-end/solution.js
+++ My-head-is-on-the-wrong-end/solution.js
@@ -2,9 +2,5 @@
 
 
 function fixTheMeerkat(arr) {
-  var animal = new Array;
-  for (var i = input.length - 1; i >= 0; i --){
-    animal.push(input[i]);
-  }
-  return animal;
+  return arr.reverse();
 };
