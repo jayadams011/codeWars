93de9d43456cf891c619c36acbe41bc8d4c2939c
jayadams011 <j.adams011@hotmail.com>
2018-01-09 21:29:31 -0800

    "added readme.md.  testing out js code."

--- My-head-is-on-the-wrong-end/solution.js
+++ My-head-is-on-the-wrong-end/solution.js
@@ -1,3 +1,10 @@
+'use strict';
+
+
 function fixTheMeerkat(arr) {
-    //your code here 
-   }
+  var animal = new Array;
+  for (var i = input.length - 1; i >= 0; i --){
+    animal.push(input[i]);
+  }
+  return animal;
+};
